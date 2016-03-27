84e386da2443e69e265732bfef8ea840ca883b8b
Edin Pjanic <pjanicedin87@gmail.com>
2016-03-27 18:37:11 +0200

    Test orientation

--- scripts/common.js
+++ scripts/common.js
@@ -90,3 +90,10 @@ $(document).ready(function(){
 });
 
 
+$(window).bind("orientationchange", function(){
+    var orientation = window.orientation;
+    var new_orientation = (orientation) ? 0 : 180 + orientation;
+    $('body').css({
+        "-webkit-transform": "rotate(" + new_orientation + "deg)"
+    });
+});
