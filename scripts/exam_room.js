8d3642cd79b09f00423168b0b069018d59507188
Edin Pjanic <edin.pjanic@upandrunningsoftware.com>
2016-04-14 13:52:41 +0200

    Add countdown timer

--- scripts/exam_room.js
+++ scripts/exam_room.js
@@ -1,7 +1,30 @@
 var exam_room_events = {
-  enter_room: show_exam_room
+  enter_room: show_exam_room,
+  countdown: start_counter
 };
 
 function show_exam_room() {
   $('.container .exam-room').show();
+  setImageSize();
+}
+
+function start_counter() {
+  var start_time = (new Date()).getTime();
+  var seconds_left;
+  var timer = window.setInterval(function(){
+    var now = (new Date()).getTime();
+    seconds_left = 1800 - (now - start_time) / 1000;
+    minutes_left = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left - (minutes_left * 60));
+    if (seconds - 10 < 0) {
+      seconds = '0' + seconds;
+    }
+    display_time(minutes_left, seconds);
+  });
+}
+
+function display_time(minutes, seconds) {
+  $clock = $('.exam-room .clock .content');
+  $clock.find('.minutes').text(minutes);
+  $clock.find('.seconds').text(seconds);
 }
