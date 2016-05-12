9e5bd4939be1107e93deea9bce9ad934ea7b1956
Edin Pjanic <pjanicedin87@gmail.com>
2016-05-12 12:32:49 +0200

    Small change

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,14 +1,16 @@
-/* Please set following variables here */
+$(document).ready(function() {
+	/* Please set following variables here */
 
-// main_variables.language
-// Describes language used accross application
-// Default: main_variables.language = 'en'
+	// main_variables.language
+	// Describes language used accross application
+	// Default: main_variables.language = 'en'
 
-// main_variables.employee_name
-// Name of person who is taking the courses
-// Default: main_variables.language = 'Test name'
+	// main_variables.employee_name
+	// Name of person who is taking the courses
+	// Default: main_variables.language = 'Test name'
 
-// lobby_events.finished_case_studies
-// Array of shortcodes for case studies that employee already finished
-// Default: lobby_events.finished_case_studies = []
-// Possible values: 'iphone', 'phone', 'mouse', 'poster1', 'poster2'
+	// lobby_events.finished_case_studies
+	// Array of shortcodes for case studies that employee already finished
+	// Default: lobby_events.finished_case_studies = []
+	// Possible values: 'iphone', 'phone', 'mouse', 'poster1', 'poster2'
+});
