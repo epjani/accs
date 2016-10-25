27f3e0170e167399686077a2933ce7481679d12b
Edin Pjanic <edin.pjanic@upandrunningsoftware.com>
2016-10-25 20:24:45 +0200

    Change email header

--- scripts/configurations/questions.js
+++ scripts/configurations/questions.js
@@ -433,7 +433,7 @@ var MOUSE = {
 		premium_products: {
 			scenarios: [
 				{
-					from: "Air Canada",
+					from: "A Customer",
 					topic: "A question for You?",
 					text: "Hello, this is Alexi from the Air Canada concierge office.  We wanted to let you know that we were able to assist Mr. & Mrs. Jones in making their connection. We were happy to help.",
 					sound: 'premium_products_mouse_1',
@@ -499,7 +499,7 @@ var MOUSE = {
 					]
 				},
 				{
-					from: "Air Canada",
+					from: "A Customer",
 					topic: "A question for You?",
 					text: "Hi.  Thanks for issuing my Latitude fare ticket to Vancouver.  I have a few questions I hope you can answer.  I wasn’t sure what complimentary Priority services I’m entitled to. Can you remind me, please? Also, we have employees that will be travelling on a regular basis for the next several months.  Are there any Business Class bulk ticket purchase options we should know about?  Lastly, what is the criterion to make Super Elite 100K this year?",
 					sound: 'premium_products_mouse_2',
@@ -569,7 +569,7 @@ var MOUSE = {
 		leisure_group: {
 			scenarios: [
 				{
-					from: "Air Canada",
+					from: "A Customer",
 					topic: "A question for You?",
 					text: "Hello. I wanted to send you a quick note to thank you for organizing our great trip on Air Canada rouge.  We are looking forward to it.  My husband is a bit of airplane buff, and I know he will ask me what types of aircraft Air Canada rouge fly. Can you tell me about that?",
 					sound: 'leisure_group_mouse_1',
@@ -639,7 +639,7 @@ var MOUSE = {
 					]
 				},
 				{
-					from: "Air Canada",
+					from: "A Customer",
 					topic: "A question for You?",
 					text: "I need your help in deciding on some vacation plans. I’ve been considering a cruise.  Can I book both my flight and cruise with Air Canada Vacations? If I choose to do a resort instead, can you tell me how I could easily compare the quality and value of the different hotels and resort options?",
 					sound: 'leisure_group_mouse_2',
@@ -708,7 +708,7 @@ var MOUSE = {
 		network_fleet: {
 			scenarios: [
 				{
-					from: "Air Canada",
+					from: "A Customer",
 					topic: "A question for You?",
 					text: "Hi. I hope this email finds you well.  I saw some pretty amazing pricing for Air Canada’s business class, and I’m considering it for my next trip.  Can you give me some details on what I should expect?  Thanks.",
 					sound: 'network_fleet_mouse_1',
